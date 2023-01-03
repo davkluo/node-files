d52bd01b6a71dc355e7330d649a2f3968c19ae88
David Luo <davidkluo@gmail.com>
2023-01-03 11:45:51 -0800

    Finish step 1

---
+++ step1.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const fsP = require('fs/promises');
+
+/**
+ * cat: Read a file and print its contents
+ *
+ * path: Path to a file to be read
+ */
+async function cat(path){
+  try {
+    const contents = await fsP.readFile(path, 'utf8');
+    console.log(contents);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+cat(process.argv[2]);
